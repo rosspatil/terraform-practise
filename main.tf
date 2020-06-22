57c954a8ae8dcf1ad021d251df2585c812f25276
Roshan Patil <patilroshan443@gmail.com>
2020-06-22 11:30:32 +0530

    Added S3 sample

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "neos-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+

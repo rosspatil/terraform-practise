65a405166a58b04dab8cbe23fae41204eefa427e
Roshan Patil <patilroshan443@gmail.com>
2020-06-25 12:24:47 +0530

    added S3 bucket

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
 region  = "us-east-1"
 }
 
-#resource "aws_s3_bucket" "b" {
-#  bucket = "neos-test-bucket"
-#  acl    = "private"
+resource "aws_s3_bucket" "b" {
+  bucket = "neos-test-bucket"
+  acl    = "private"
 
-#  tags = {
-#    Name        = "My bucket"
-#    Environment = "Dev"
-#  }
-#}
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
